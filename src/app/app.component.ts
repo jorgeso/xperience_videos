a10a4b4daa62f17f6ddd866306ba412736b1097e
Jorge <jneyrav@hotmail.com>
2024-05-11 06:37:46 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,39 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { BlobServiceClient } from '@azure/storage-blob';
-import { DefaultAzureCredential } from '@azure/identity'
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule, CommonModule, MatListModule, MatCardModule, MatButtonModule, MatIconModule],
   templateUrl: './app.component.html',
   styles: [],
 })
 export class AppComponent {
   title = 'xperience_videos';
-  blobService: BlobServiceClient;
-  constructor(){
-    const defaultAzureCredentials = new DefaultAzureCredential()
-    this.blobService = new BlobServiceClient(
-      "https://xperiencestore.blob.core.windows.net",
-      defaultAzureCredentials
-    )
+  tournaments: any;
+  selectedTournament: any;
+  constructor(private http: HttpClient) {
   }
-  async ngOnInit(){
-    const containerClient = this.blobService.getContainerClient("tournaments")
-    let i = 1;
-    for await (const blob of containerClient.listBlobsFlat()) {
-      console.log(`Blob ${i++}: ${blob.name}`);
-    }
+  async ngOnInit() {
+    this.http.get("/api/videos").subscribe((tournaments: any) => {
+      this.tournaments = tournaments
+    })
 
+  }
+
+  goToTournament(tournamentName: string) {
+    this.selectedTournament = this.tournaments.find((tournament: any) => tournament.tournamentName == tournamentName)
+  }
+
+  goToTournaments() {
+    this.selectedTournament = null
   }
 }
--- src/server/routes.js
+++ src/server/routes.js
@@ -1,10 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const { DefaultAzureCredential } = require("@azure/identity");
+const { BlobServiceClient } = require("@azure/storage-blob");
 
-router.get('/videos', (req, res) => {
- res.send(200, [
-    {"name": "game", "url": "test"}
- ])
+router.get('/videos', async (req, res) => {
+  const defaultAzureCredentials = new DefaultAzureCredential()
+  blobService = new BlobServiceClient(
+    "https://xperiencestore.blob.core.windows.net",
+    defaultAzureCredentials
+  )
+  const containerClient = blobService.getContainerClient("tournaments")
+  let i = 1;
+  const videosDict = {}
+  for await (const blob of containerClient.listBlobsFlat()) {
+    const blobClient = containerClient.getBlobClient(blob.name)
+    parts = blob.name.split("/")
+    tournamentName = parts[0]
+    game = parts[1].split(".")[0]
+    if (!(tournamentName in videosDict)) {
+      videosDict[tournamentName] = {
+        tournamentName: tournamentName,
+        games: []
+      }
+    }
+    const video = {
+      game: game,
+      url: blobClient.url
+    }
+    videosDict[tournamentName].games.push(video)
+    
+  }
+  res.send(200, Object.values(videosDict))
 });
 
-module.exports=router;
+module.exports = router;
